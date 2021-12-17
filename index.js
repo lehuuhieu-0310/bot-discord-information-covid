6f56a94de03d1304476d2465fab101a605e5d2da
Le Huu Hieu <01643815296h@gmail.com>
2021-12-17 12:56:36 +0700

    add new slash commands

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { Client, Intents } = require('discord.js')
+const { Client, Intents, MessageEmbed } = require('discord.js')
 const axios = require('axios').default
+const schedule = require('node-schedule')
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 require('dotenv').config()
 
@@ -7,23 +9,89 @@ client.login(process.env.TOKEN)
 
 client.on('ready', () => {
     console.log('ready!')
+
+    //id your server
+    const guildId = process.env.GUILD_ID
+    const guild = client.guilds.cache.get(guildId)
+
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    })
+    commands?.create({
+        name: 'server',
+        description: 'Replies with server info'
+    })
+    commands?.create({
+        name: 'user',
+        description: 'Replies with user info'
+    })
+    commands?.create({
+        name: 'covid',
+        description: 'Replies with information of covid in VietNam'
+    })
 })
 
-client.on('messageCreate', message => {
-    if (message.content === 'covid') {
-        setInterval(async () => {
-            const data = await axios.get(process.env.API)
-            const dataCovid = data.data.CovidVN
-            message.channel.send(`
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return
+
+    const { commandName } = interaction
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!')
+    } else if (commandName === 'server') {
+        await interaction.reply('Server info.')
+    } else if (commandName === 'user') {
+        await interaction.reply(`${interaction.user.username}`)
+    } else if (commandName === 'covid') {
+        await interaction.deferReply()
+        const data = await axios(process.env.API)
+        const dataCovid = data.data.CovidVN
+        await interaction.editReply(`
             ${dataCovid.CapNhat}
-            Hôm nay: 
+            - Hôm nay: 
                 Nhiễm: ${dataCovid.HomNay.nhiem}
                 Khỏi: ${dataCovid.HomNay.khoi}
                 Tử vong: ${dataCovid.HomNay.tuvong}
-            Tổng:
+            - Tổng:
                 Nhiêm: ${dataCovid.Tong.nhiem}
                 Khỏi: ${dataCovid.Tong.khoi}
-                Tử vong: ${dataCovid.Tong.tuvong}`)
-        }, 86400000)
+                Tử vong: ${dataCovid.Tong.tuvong}
+        `)
+    }
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.content === '!covid') {
+        message.reply('Scheduled Task')
+        schedule.scheduleJob({ tz: 'Asia/Bangkok', hour: 19 }, async () => {
+            const data = await axios(process.env.API)
+            const dataCovid = data.data.CovidVN
+
+            const embed = new MessageEmbed()
+                .setColor('#F1B199')
+                .setTitle(`Covid-19 tại Việt Nam: ${dataCovid.CapNhat}`)
+                .setDescription(`
+                - Hôm nay: 
+                    Nhiễm: ${dataCovid.HomNay.nhiem}
+                    Khỏi: ${dataCovid.HomNay.khoi}
+                    Tử vong: ${dataCovid.HomNay.tuvong}
+                - Tổng:
+                    Nhiêm: ${dataCovid.Tong.nhiem}
+                    Khỏi: ${dataCovid.Tong.khoi}
+                    Tử vong: ${dataCovid.Tong.tuvong}
+            `)
+                .setTimestamp()
+                .setFooter('Data is crawl from vnexpress.vn')
+
+            message.channel.send({ embeds: [embed] })
+        })
     }
 })
